e15a9d3e08b45f60f56598b07cdba9f815ea55a7
IsaacCastelo <isaacpxl8@gmail.com>
2023-05-12 21:01:43 -0700

    ArregloClases
    si

---
+++ AhorcadoJugador/src/main/java/modelo/Modelo.java
@@ -0,0 +1,19 @@
+
+package modelo;
+
+import vista.Partida;
+
+public class Modelo {
+    
+    public void actualizar(Partida partida){
+        
+    }
+    
+    public Partida obtenerModelo(){
+        return null;
+    }
+    
+    public void enviarJugada(char letraIngresada){
+        
+    }
+}
